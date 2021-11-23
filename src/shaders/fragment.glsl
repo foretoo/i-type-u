d6c7421f80eece57e4a31382b9d95716cd702066
Sergey Borovikov <serg.there@gmail.com>
2021-11-24 01:03:59 +0300

    ~

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,6 +1,6 @@
 varying float vNoise;
 varying vec2 vUv;
-uniform sampler2D cloudTexture;
+// uniform sampler2D cloudTexture;
 uniform float time;
 
 void main() {
@@ -8,14 +8,14 @@ void main() {
   vec3 greenColor = vec3(0., 1., 0.);
   vec3 finalColor = mix(blueColor, greenColor, 0.5*(vNoise + 1.));
 
-  vec2 newUV = vUv;
+  // vec2 newUV = vUv;
   // newUV = vec2(newUV.x, newUV.y*.8 + .1 + .1*sin(newUV.x*5. + time*.1));
 
-  vec4 cloud = texture2D(cloudTexture, newUV);
-  float newvNoise = vNoise;
+  // vec4 cloud = texture2D(cloudTexture, newUV);
+  // float newvNoise = vNoise;
 
-  // gl_FragColor = vec4(finalColor, 1.0);
+  gl_FragColor = vec4(finalColor, 1.0);
   // gl_FragColor = vec4(vUv, 0.0, 1.0);
   // gl_FragColor = vec4(vNoise);
-  gl_FragColor = cloud * newvNoise;
+  // gl_FragColor = cloud * newvNoise;
 }
