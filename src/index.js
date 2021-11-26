1247708803d06a2ad538e44ff28d096a4daf20e3
Sergey Borovikov <serg.there@gmail.com>
2021-11-26 03:15:33 +0300

    add: time and imageTexture uniforms

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import * as THREE from 'three'
 import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
+import './index.css'
+import image from '../assets/bernoulli.png'
 
 
 
+let time = 0
 const [ width, height ] = [ document.documentElement.clientWidth, document.documentElement.clientHeight ]
 
 const scene = new THREE.Scene();
@@ -18,18 +21,21 @@ window.root.appendChild( renderer.domElement )
 const geometry = new THREE.PlaneBufferGeometry( 1,1.25, 120,150 )
 const material = new THREE.ShaderMaterial({
   uniforms: {
-    time: { value: 0 }
+    time: { value: 0 },
+    bernoulli: { value: new THREE.TextureLoader().load(image)}
   },
   fragmentShader: fragment,
   vertexShader: vertex
 })
 const obj = new THREE.Mesh( geometry, material )
-scene.add( obj );
+scene.add( obj )
 
 
 
 function render() {
-  material.uniforms.time.value = performance.now() / 1000
+  time = performance.now() / 1000
+  obj.rotation.x = (Math.sin(time) - 15) / 14
+  material.uniforms.time.value = time
   renderer.render( scene, camera )
   requestAnimationFrame( render )
 }
