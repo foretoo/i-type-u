95806297856eb62eef1d7bb63c920e9de758e864
Sergey Borovikov <serg.there@gmail.com>
2021-12-01 03:08:04 +0300

    some usefull reconfigs

--- src/index.js
+++ src/index.js
@@ -2,32 +2,45 @@ import * as THREE from 'three'
 import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
 import './index.css'
-import image from '../assets/bernoulli.png'
 
+
+
+//////// CANVAS ////////
 
 const pd = window.devicePixelRatio
 const canvas = document.createElement('canvas')
 const ctx = canvas.getContext('2d')
-canvas.width = 128 * pd
-canvas.height = 160 * pd
-canvas.style.cssText = `width: ${canvas.width / pd}px; height: ${canvas.height / pd}px;`
-ctx.fillStyle = 'white'
+canvas.width = 256 * pd
+canvas.height = 320 * pd
+canvas.style.cssText = `width: ${canvas.width / 2}px; height: ${canvas.height / 2}px;`
+ctx.fillStyle = '#dddddd'
 ctx.fillRect(0, 0, canvas.width, canvas.height)
-ctx.font = '20px serif'
+ctx.font = '40px serif'
 const texture = new THREE.CanvasTexture(canvas)
 
 
+
+//////// INPUT ////////
+
 let textInput = ''
+const rect = { w: 0, h: 0 }
 const input = document.createElement('input')
 input.oninput = e => {
   textInput = e.target.value
-  ctx.fillStyle = 'white'
+  ctx.fillStyle = '#dddddd'
   ctx.fillRect(0, 0, canvas.width, canvas.height)
+  rect.w = Math.abs(ctx.measureText(textInput).actualBoundingBoxLeft) + Math.abs(ctx.measureText(textInput).actualBoundingBoxRight)
+  rect.h = ctx.measureText(textInput).fontBoundingBoxAscent + ctx.measureText(textInput).fontBoundingBoxDescent
+  ctx.fillStyle = 'white'
+  ctx.fillRect(0, 0, rect.w, rect.h)
   ctx.fillStyle = 'blue'
-  ctx.fillText(textInput, 0, 24)
+  ctx.fillText(textInput, 0, ctx.measureText(textInput).fontBoundingBoxAscent)
   texture.needsUpdate = true
 }
 
+
+
+//////// THREE ////////
 
 let time = 0
 const [ width, height ] = [ document.documentElement.clientWidth, document.documentElement.clientHeight ]
@@ -50,12 +63,6 @@ const material = new THREE.ShaderMaterial({
 const obj = new THREE.Mesh( geometry, material )
 scene.add( obj )
 
-
-document.body.appendChild( renderer.domElement )
-document.body.appendChild( canvas )
-document.body.appendChild( input )
-
-
 function render() {
   time = performance.now() / 1000
   obj.rotation.x = (Math.sin(time) - 11) / 13
@@ -66,4 +73,7 @@ function render() {
 
 
 
+document.body.appendChild( renderer.domElement )
+document.body.appendChild( canvas )
+document.body.appendChild( input )
 render()
