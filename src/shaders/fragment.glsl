9b58948a60408ccdbcccb5da4dc36f34d7035196
Sergey Borovikov <serg.there@gmail.com>
2021-12-10 09:32:16 +0300

    add random

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -2,9 +2,15 @@ varying float vNoise;
 varying vec2 vUv;
 uniform sampler2D image;
 
+float random (vec2 st) {
+  return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
 void main() {
+
+  float rnd = random(vUv) * 0.4;
 
   vec4 text = texture2D(image, vUv);
 
-  gl_FragColor = text * vNoise;
+  gl_FragColor = text - pow(1.0 - vNoise, 2.0) + rnd;
 }
