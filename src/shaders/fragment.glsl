8c0ce39076c50a4e663397f8f9a09be613310f96
Sergey Borovikov <serg.there@gmail.com>
2021-12-15 18:41:24 +0300

    fabrick-dispaly texture

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -8,9 +8,9 @@ float random (vec2 st) {
 
 void main() {
 
-  float rnd = random(vUv) * 0.4;
+  float rnd = random(vUv) * abs(sin(vUv.x * 512.0)) * abs(sin(vUv.y * 640.0));
 
   vec4 text = texture2D(image, vUv);
 
-  gl_FragColor = text - pow(1.0 - vNoise, 2.0) + rnd;
+  gl_FragColor = text + rnd - pow(1.0 - vNoise, 2.0);
 }
