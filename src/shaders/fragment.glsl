b2847297216cbd8c4784cf73492f56dfde446240
Sergey Borovikov <serg.there@gmail.com>
2021-12-16 23:55:31 +0300

    ~

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -6,11 +6,16 @@ float random (vec2 st) {
   return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
 }
 
+float smootherstep(float edge0, float edge1, float x) {
+  x = clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
+  return x * x * x * (x * (x * 6.0 - 15.0) + 10.0);
+}
+
 void main() {
 
   float rnd = random(vUv) * abs(sin(vUv.x * 512.0)) * abs(sin(vUv.y * 640.0));
 
   vec4 text = texture2D(image, vUv);
 
-  gl_FragColor = text + rnd - pow(1.0 - vNoise, 2.0);
+  gl_FragColor = text + rnd - smootherstep(0.0, 1.0, 0.8 - vNoise);
 }
